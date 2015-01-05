73bae70f0c01f748ceca831b86e2c84e5009eaac
omidontop <omidontop@gmail.com>
2015-01-05 22:38:02 +0100

    - Added a build.mk for stm32f10x_drv. - Defined the stack and heap sizes in config.h.

--- src/app/include/config.h
+++ src/app/include/config.h
@@ -1,6 +1,39 @@
 /*******************************************************************************
-**
+*
+* Name: config.h
+* Description: a header file containing various configurations of the
+* 	components as well as the application itself.
+*
+********************************************************************************
+*
+* 	Copyright (C) 2015 by Omid Manikhi
+*
 *******************************************************************************/
 
+#ifndef __CONFIG_H
+#define __CONFIG_H
 
-#define __START main
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/*
+** Specify the entry point of the application i.e the main() function.
+*/
+#define __START				main
+
+/*
+** Specify the Stack Size.
+*/
+#define __STACK_SIZE		2048
+
+/*
+** Specify the Heap Size.
+*/
+#define __HEAP_SIZE			4096
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CONFIG_H */
